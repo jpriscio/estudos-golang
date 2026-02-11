d45adec3cb9945b810fd7021a8166eba4ee74b7f
Joao <priscioj@gmail.com>
2026-02-10 22:02:20 -0300

    iniciando gin

--- Gin/main.go
+++ Gin/main.go
@@ -14,7 +14,7 @@ func main() {
 	router.GET("/user/:name/:action", func(ctx *gin.Context) {
 		name := ctx.Param("name")
 		action := ctx.Param("action")
-		ctx.String(http.StatusOK, "Hello my friend %v parabens pela sua ação de %v", name, action)
+		ctx.String(http.StatusOK, "Hello %v parabens pela sua ação de %v", name, action)
 	})
 
 	if err := router.Run(":8080"); err != nil {
